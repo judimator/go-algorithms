ea7b2eb060a43529d545a2c743f9656050951270
Dmitry Dzhumagaliev <judimator@gmail.com>
2021-08-13 17:30:43 +0300

    Added quick sort.

--- algorithms/quicksort/quick_sort.go
+++ algorithms/quicksort/quick_sort.go
@@ -1,1 +1,23 @@
 package quicksort
+
+func Sort(arr []int, low, high int) {
+	if low < high {
+		i := swap(arr, low, high)
+		Sort(arr, low, i-1)
+		Sort(arr, i+1, high)
+	}
+}
+
+func swap(arr []int, low, high int) int {
+	pivot := arr[high]
+	i := low
+	for j := low; j < high; j++ {
+		if arr[j] < pivot {
+			arr[i], arr[j] = arr[j], arr[i]
+			i++
+		}
+	}
+	arr[i], arr[high] = arr[high], arr[i]
+
+	return i
+}
--- algorithms/quicksort/quick_sort_test.go
+++ algorithms/quicksort/quick_sort_test.go
@@ -1,1 +1,23 @@
 package quicksort
+
+import (
+	"reflect"
+	"testing"
+)
+
+var tests = [][][]int{
+	{
+		[]int{1, 2, 5, 3, 1, 6, 4, 9, 6, 5},
+		[]int{1, 1, 2, 3, 4, 5, 5, 6, 6, 9},
+	},
+}
+
+func TestSort(t *testing.T) {
+	for _, test := range tests {
+		Sort(test[0], 0, len(test[0])-1)
+
+		if !reflect.DeepEqual(test[1], test[0]) {
+			t.Error("Invalid array order!")
+		}
+	}
+}
